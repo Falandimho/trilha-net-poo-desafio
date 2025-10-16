e4b30697117950f2427412f1214f36bf7656bb41
Falandimho <anthonygames311@gmail.com>
2025-10-16 15:53:58 -0300

    implementando as funcionalidades e testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste com o nokia");
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+Console.WriteLine("Teste com o Iphone");
+Iphone iphone = new Iphone("987654321", "iPhone 13", "654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
